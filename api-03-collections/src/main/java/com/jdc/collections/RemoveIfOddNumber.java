6d4eccbc58ba5b39fd842f41e5d10e703711db7d
zinlinhtetfixie <zin.lin.htet@fixie.jp>
2024-07-26 15:48:20 +0630

    using iterator and sorting elements

--- api-03-collections/src/main/java/com/jdc/collections/RemoveIfOddNumber.java
+++ api-03-collections/src/main/java/com/jdc/collections/RemoveIfOddNumber.java
@@ -1,18 +1,31 @@
 package com.jdc.collections;
 
-import java.util.Iterator;
 import java.util.List;
-
-import lombok.experimental.var;
 
 public class RemoveIfOddNumber {
 
 	public List<Integer> removeIfOdd(List<Integer> list) {
 
-		for (int i = 0; i < list.size(); i++) {
-			var item = list.get(i);
+//		for (int i = 0; i < list.size(); i++) {
+//			var item = list.get(i);
+//			if (item % 2 != 0) {
+//				list.remove(item);
+//			}
+//		}
+
+//		for (var item : list) {
+//			var index = list.indexOf(item);
+//
+//			if (item % 2 != 0) {
+//				list.remove(index);
+//			}
+//		}
+
+		var iterator = list.iterator();
+		while (iterator.hasNext()) {
+			var item = iterator.next();
 			if (item % 2 != 0) {
-				list.remove(item);
+				iterator.remove();
 			}
 		}
 
