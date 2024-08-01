ef600a5994d2b79d4e22ac885fa45d2d3a7c8f2a
zinlinhtetfixie <zin.lin.htet@fixie.jp>
2024-08-01 16:13:09 +0630

    about filter and takewhile dropwhile of stream

---
+++ api-04-lambda-and-streams/ep04-intermediate-operations/src/test/java/com/jdc/stream/TakeWhileAndDropWhileTest.java
@@ -0,0 +1,20 @@
+package com.jdc.stream;
+
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+public class TakeWhileAndDropWhileTest {
+
+	@Test
+	void test_take_while() {
+		System.out.println("Take While");
+		IntStream.iterate(1, a -> a + 1).takeWhile(a -> a <= 10).forEach(System.out::println);
+	}
+
+	@Test
+	void test_drop_while() {
+		System.out.println("Drop While");
+		IntStream.iterate(1, a -> a + 1).dropWhile(a -> a < 10).limit(10).forEach(System.out::println);
+	}
+}
